5615a45850be901bbc955329774ecfc908bf936c
RJ Robinson <robert_robinson@cable.comcast.com>
2017-07-03 09:58:42 -0400

    bump

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import $ from 'jquery';
+
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyD8mqklyFv_oJ9hyLdX2JvjyAH8lnMNP-c'
+
+// Create a new component
+// Should produce some HTML
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
